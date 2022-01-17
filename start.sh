f9b7c94822597f4b0ad1e45bd23f4b9c514eac25
Edwin Bautista <edwin.bautista@glocation.com.co>
2022-01-17 18:14:03 -0500

    bin bash

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ echo "Mounting completed."
 # For environments with multiple CPU cores, increase the number of workers
 # to be equal to the cores available.
 # Timeout is set to 0 to disable the timeouts of the workers to allow Cloud Run to handle instance scaling.
-exec xpra start --start=qgis --bind-tcp=0.0.0.0:8080 --html=on && tail -f /dev/null &
+/bin/bash xpra start --start=qgis --bind-tcp=0.0.0.0:8080 --html=on && tail -f /dev/null
 
 # Exit immediately when one of the background processes terminate.
-wait -n
+# wait -n
